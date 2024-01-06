462950e2ba104ab8ce01999941e1d4a257470455
harun-mese <harunmese13@gmail.com>
2024-01-06 03:21:45 +0300

    bottom navigation completed

--- app/src/main/java/www/harunmese/dev/mapsandpins/MainActivity.kt
+++ app/src/main/java/www/harunmese/dev/mapsandpins/MainActivity.kt
@@ -2,24 +2,28 @@ package www.harunmese.dev.mapsandpins
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import www.harunmese.dev.mapsandpins.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        val navController = findNavController(binding.fragmentContainerView)
-        // Find reference to bottom navigation view
-        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        // Hook your navigation controller to bottom navigation view
-       navView.setupWithNavController(navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        navView.setupWithNavController(navController)
+
 
     }
 }
